eb3cca7f33c11427e44e0d0f6d6ef32177b5e2b4
Elmer <elmerpalencia101@gmail.com>
2024-04-13 01:17:23 -0500

    modified mr. america

--- mrA.js
+++ mrA.js
@@ -1,16 +1,17 @@
-let currentXP = 0;
-const maxXp = 200;
+// let currentXP = 0;
+// const maxXp = 200;
 
-function increaseXP(){
-    currentXP += 1;
-    if (currentXP >= maxXP){
-        currentXP = maxXP;
-    }
-    updateXPBar();
-}
+// function increaseXP(){
+//     currentXP += 1;
+//     if (currentXP >= maxXP){
+//         currentXP = maxXP;
+//     }
+//     updateXPBar();
+// }
 
-function updateXPBar(){
-        const xpBar = document.getElementById("xp-bar");
-        const percentage = (currentXP / maxXP) * 100;
-        xpBar.style.width = percentage + "%";
-}
+// function updateXPBar(){
+//         const xpBar = document.getElementById("xp-bar");
+//         const percentage = (currentXP / maxXP) * 100;
+//         xpBar.style.width = percentage + "%";
+// }
+
